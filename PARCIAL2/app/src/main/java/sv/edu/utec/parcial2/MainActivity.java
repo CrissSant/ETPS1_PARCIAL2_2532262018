54687e3f64e8fdbe6cadb1cdd1b9072e73b6e73f
Cristian Santamaria <124642195+CrissSant@users.noreply.github.com>
2023-03-22 21:40:10 -0600

    Parcial2

--- PARCIAL2/app/src/main/java/sv/edu/utec/parcial2/MainActivity.java
+++ PARCIAL2/app/src/main/java/sv/edu/utec/parcial2/MainActivity.java
@@ -25,6 +25,7 @@ AdaptadorPersonas adaptadorPersonas;
         mrPersonas.setLayoutManager(new LinearLayoutManager(this));
         mrPersonas.setAdapter(adaptadorPersonas);
         adaptadorPersonas=new AdaptadorPersonas(this,getListado());
+        mrPersonas.setAdapter(adaptadorPersonas);
     }
 
     private ArrayList<Modelo> getListado()
@@ -35,66 +36,77 @@ AdaptadorPersonas adaptadorPersonas;
         persona.setCargo("CEO");
         persona.setCompania("Insures S.O");
         persona.setFoto(R.drawable.lead_photo_1);
+        modelos.add(persona);
 
         persona  = new Modelo();
         persona.setNombre("Carlos Lopez");
         persona.setCargo("Asistente");
         persona.setCompania("Hospital blue");
         persona.setFoto(R.drawable.lead_photo_2);
+        modelos.add(persona);
 
         persona  = new Modelo();
         persona.setNombre("Sara Bonz");
         persona.setCargo("Directora de Marketing");
         persona.setCompania("Electrical Parts Ltd");
         persona.setFoto(R.drawable.lead_photo_3);
+        modelos.add(persona);
 
         persona  = new Modelo();
         persona.setNombre("Liliana Clarence");
         persona.setCargo("Disenadora de producto");
         persona.setCompania("Creativa App");
         persona.setFoto(R.drawable.lead_photo_4);
+        modelos.add(persona);
 
         persona  = new Modelo();
         persona.setNombre("Benito Peralta");
         persona.setCargo("Supervisor de ventas");
         persona.setCompania("Neomaticos Press");
         persona.setFoto(R.drawable.lead_photo_5);
+        modelos.add(persona);
 
         persona  = new Modelo();
         persona.setNombre("Juan Jamarillo");
         persona.setCargo("CEO");
         persona.setCompania("Banco Nacional");
         persona.setFoto(R.drawable.lead_photo_6);
+        modelos.add(persona);
 
         persona  = new Modelo();
         persona.setNombre("Christian Steps");
         persona.setCargo("CTO");
         persona.setCompania("Cooperativa verde");
         persona.setFoto(R.drawable.lead_photo_7);
+        modelos.add(persona);
 
         persona  = new Modelo();
         persona.setNombre("Alexa Giraldo");
         persona.setCargo("Lead Programmer");
         persona.setCompania("Frustisofy");
         persona.setFoto(R.drawable.lead_photo_8);
+        modelos.add(persona);
 
         persona  = new Modelo();
         persona.setNombre("Linda Murillo");
         persona.setCargo("Directora de Marketing");
         persona.setCompania("Seguros Boliver");
         persona.setFoto(R.drawable.lead_photo_9);
+        modelos.add(persona);
 
         persona  = new Modelo();
         persona.setNombre("Lizeth Astrada");
         persona.setCargo("CEO");
         persona.setCompania("Concensionari Molotox");
         persona.setFoto(R.drawable.lead_photo_10);
+        modelos.add(persona);
 
         persona  = new Modelo();
         persona.setNombre("Cristian Santamaria");
         persona.setCargo("Estudiante");
         persona.setCompania("Universidad Tecnologica de El Salvador");
         persona.setFoto(R.drawable.yo);
+        modelos.add(persona);
 
         return modelos;
     }
